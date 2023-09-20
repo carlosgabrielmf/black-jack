873f760cfd4d73892009d5a7c241035c85f03fd7
carlosgabrielmf <carlosg.mosquera12@gmail.com>
2023-09-20 18:35:25 +0200

    use strict and module pattern v1.0.1

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,159 +1,156 @@
-// VARIABLES GLOBALES
-let deck = []; // Creamos un array vacio porque vamos a enviarle datos desde una funcion
-let types = ["C", "D", "H", "S"]; // Aqui lo que hacemos es guardar lo tipos de carta
-const specials = ["A", "j", "Q", "K"]; // Aqui creamos un areglo para cartas especiales
+(() => {
+  "use strict";
+  // VARIABLES GLOBALES
+  let deck = []; // Creamos un array vacio porque vamos a enviarle datos desde una funcion
+  let types = ["C", "D", "H", "S"]; // Aqui lo que hacemos es guardar lo tipos de carta
+  const specials = ["A", "j", "Q", "K"]; // Aqui creamos un areglo para cartas especiales
 
-// REFERENCIAS HTML5
-const btnGet = document.querySelector('#btn-get');
-const btnStop = document.querySelector('#btn-stop');
-const btnNewGame = document.querySelector('#btn-new');
+  // REFERENCIAS HTML5
+  const btnGet     = document.querySelector("#btn-get");
+  const btnStop    = document.querySelector("#btn-stop");
+  const btnNewGame = document.querySelector("#btn-new");
 
-let playerPoints = 0, 
+  let playerPoints = 0,
     computerPoints = 0;
 
-const htmlPoints = document.querySelectorAll('small');
+  const htmlPoints = document.querySelectorAll("small");
 
-const playerCards =  document.querySelector('#player-cards');
-const computerCards =  document.querySelector('#computer-cards');
- 
-// FUNCION PARA BARAJAR CARTAS
-const createDeck = () => {
-  for (let i = 2; i < 10; i++) {
-    // Creamos un bucle for of que recorra los "types"
+  const playerCards = document.querySelector("#player-cards");
+  const computerCards = document.querySelector("#computer-cards");
+
+  // FUNCION PARA BARAJAR CARTAS
+  const createDeck = () => {
+    for (let i = 2; i < 10; i++) {
+      // Creamos un bucle for of que recorra los "types"
+      for (let type of types) {
+        // Introducimos los tipos en el arreglo de las baraja o "deck" mediante metodo push
+        deck.push(i + type);
+      }
+    }
+
+    // Recorremos el tipo de tipos para poder obtener cada uno
     for (let type of types) {
-      // Introducimos los tipos en el arreglo de las baraja o "deck" mediante metodo push
-      deck.push(i + type);
+      // Recorremos la carta especiales de las especiales para obtener cada una
+      for (let special of specials) {
+        // Introducimos una carta especial por cada tipo
+        deck.push(special + type);
+      }
     }
-  }
+    // Aqui utilizamos una libreria de terceros llamada: Underscore
+    deck = _.shuffle(deck);
+  };
 
-  // Recorremos el tipo de tipos para poder obtener cada uno
-  for (let type of types) {
-    // Recorremos la carta especiales de las especiales para obtener cada una
-    for (let special of specials) {
-      // Introducimos una carta especial por cada tipo
-      deck.push(special + type);
+  createDeck();
+
+  // FUNCION PARA PEDIR CARTAS
+  const getCard = () => {
+    if (deck.length === 0) {
+      throw "No hay carta en le deck";
     }
-  }
-  // Aqui utilizamos una libreria de terceros llamada: Underscore
-  deck = _.shuffle(deck);
-  console.log(deck);
-};
 
-createDeck();
+    // El metodo pop nos regresa el ultimo elemento de un array y luego lo borra del arreglo
+    const card = deck.pop();
 
-// FUNCION PARA PEDIR CARTAS
-const getCard = () => {
-  if (deck.length === 0) {
-    throw "No hay carta en le deck";
-  }
+    return card;
+  };
 
-  // El metodo pop nos regresa el ultimo elemento de un array y luego lo borra del arreglo
-  const card = deck.pop();
+  // getCard()
+  const cardValue = (card) => {
+    const value = card.substring(0, card.length - 1);
 
-  return card;
-};
+    return isNaN(
+      value
+    ) /*pasamos por parametros los valores con operadores ternarios */
+      ? value === "A"
+        ? 11
+        : 10
+      : value * 1;
+  };
 
-// getCard()
-const cardValue = (card) => {
-  const value = card.substring(0, card.length - 1);
+  const value = cardValue(getCard());
+  console.log({ value });
 
- return (isNaN(value)) ? /*pasamos por parametros los valores con operadores ternarios */
-        (value === 'A') ? 11 : 10
-        : value * 1;
-};
+  //Eventos con callback y sin ellos.
 
-const value = cardValue( getCard());
-console.log({value});
+  // Evento Click: Pedir Carta.
+  btnGet.addEventListener("click", () => {
+    const card = getCard();
 
-//Eventos con callback y sin ellos.
+    playerPoints = playerPoints + cardValue(card);
 
-// Evento Click: Pedir Carta.
-btnGet.addEventListener('click', () => {
+    htmlPoints[0].innerText = playerPoints;
 
-  const card = getCard();
+    const cardsImg = document.createElement("img");
+    cardsImg.src = `assets/cards/${card.toUpperCase()}.png`;
+    cardsImg.classList.add("cards");
 
-  playerPoints = playerPoints + cardValue( card )
+    playerCards.append(cardsImg);
 
-  htmlPoints[0].innerText = playerPoints;
+    if (playerPoints > 21) {
+      console.warn("You Lose");
+      btnGet.disabled = true;
+      btnStop.disabled = true;
+      computerTurn(playerPoints);
+    } else if (playerPoints === 21) {
+      console.warn("You Win");
+      btnGet.disabled = true;
+      btnStop.disabled = true;
+      computerTurn(playerPoints);
+    }
+  });
 
-  const cardsImg = document.createElement('img');
-  cardsImg.src = `assets/cards/${card.toUpperCase()}.png`;
-  cardsImg.classList.add('cards');
-
-  playerCards.append(cardsImg)
-
-  if (playerPoints > 21) {
-    console.warn('You Lose');
+  btnStop.addEventListener("click", () => {
     btnGet.disabled = true;
     btnStop.disabled = true;
+
     computerTurn(playerPoints);
-  } else if (playerPoints === 21) {
-    console.warn('You Win');
-    btnGet.disabled = true;  
-    btnStop.disabled = true;
-    computerTurn(playerPoints);
-  }
+  });
 
-});
+  btnNewGame.addEventListener("click", () => {
+    deck = [];
+    deck = createDeck();
 
-btnStop.addEventListener('click', () => {
+    playerPoints = 0;
+    computerPoints = 0;
+    htmlPoints[0].innerText = 0;
+    htmlPoints[1].innerText = 0;
 
-  btnGet.disabled  = true;
-  btnStop.disabled = true;
+    computerCards.innerHTML = "";
+    playerCards.innerHTML = "";
 
-  computerTurn(playerPoints);
+    btnGet.disabled = false;
+    btnStop.disabled = false;
+  });
 
-});
+  // TURNO DE LA COMPUTADORA
+  const computerTurn = (minimalPoints) => {
+    do {
+      const card = getCard();
 
-btnNewGame.addEventListener('click', () => {
+      computerPoints = computerPoints + cardValue(card);
 
-  deck = [];
-  deck = createDeck();
+      htmlPoints[1].innerText = computerPoints;
 
-  playerPoints = 0;
-  computerPoints = 0;
-  htmlPoints[0].innerText = 0;
-  htmlPoints[1].innerText = 0;
+      const cardsImg = document.createElement("img");
+      cardsImg.src = `assets/cards/${card.toUpperCase()}.png`;
+      cardsImg.classList.add("cards");
+      computerCards.append(cardsImg);
 
-  computerCards.innerHTML = "";
-  playerCards.innerHTML = "";
+      if (minimalPoints > 21) {
+        break;
+      }
+    } while (computerPoints < minimalPoints && minimalPoints <= 21);
 
-  btnGet.disabled  = false;
-  btnStop.disabled = false;
-});
-
-
-// TURNO DE LA COMPUTADORA
-const computerTurn = (minimalPoints) => {
-
-  do {
-  const card = getCard();
-
-  computerPoints = computerPoints + cardValue( card )
-
-  htmlPoints[1].innerText = computerPoints;
-
-  const cardsImg = document.createElement('img');
-  cardsImg.src = `assets/cards/${card.toUpperCase()}.png`;
-  cardsImg.classList.add('cards');
-  computerCards.append(cardsImg)
-
-    if (minimalPoints > 21) {
-      break;
-    }
-
-  } while ( (computerPoints < minimalPoints ) && ( minimalPoints <= 21 ) );
-
-  setTimeout(() => {
-    if ( computerPoints === minimalPoints ) {
-      alert('Nadie gana!')
-    } else if ( minimalPoints > 21 ) {
-      alert('Computadora win!')
-    } else if ( computerPoints > 21 ) {
-      alert('Jugador win!!')
-    } else {
-      alert('Computadora win!')
-    }
-  }, 10);
-
-}
+    setTimeout(() => {
+      if (computerPoints === minimalPoints) {
+        alert("Nadie gana!");
+      } else if (minimalPoints > 21) {
+        alert("Computadora win!");
+      } else if (computerPoints > 21) {
+        alert("Jugador win!!");
+      } else {
+        alert("Computadora win!");
+      }
+    }, 10);
+  };
+})();
